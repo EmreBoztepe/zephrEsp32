cffdddabb19f2429530c883eb7373729d81d3646
Emre Boztepe <boztepeemre94@gmail.com>
2026-05-09 21:37:44 +0300

    basic wifi scan

--- src/main.c
+++ src/main.c
@@ -1,26 +1,62 @@
 #include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <string.h>
 
-#define STACK_SIZE 1024U
+static void scan_result_handler(struct net_mgmt_event_callback *cb,
+                                uint32_t mgmt_event, struct net_if *iface)
+{
+    const struct wifi_scan_result *res = (const struct wifi_scan_result *)cb->info;
+    
+    if (res)
+	{
 
-#define PRIO_THREAD_A	3U
-#define PRIO_THREAD_B	44U
+        char ssid_str[33] = {0};
+        uint8_t len = (res->ssid_length <= 32) ? res->ssid_length : 32;
+        memcpy(ssid_str, res->ssid, len);
 
-extern void Thread_B(void *, void *, void *);
-extern void Thread_A(void *, void *, void *);
+        // Dogrudan donanim uzerinden (UART) ekrana bas
+        printk("[NODE DETECTED] SSID: %-20s | RSSI: %4d dBm | Channel: %2d\n", 
+               ssid_str, res->rssi, res->channel);
+    }
+}
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE,
-				Thread_A, NULL, NULL, NULL,
-				PRIO_THREAD_A, 0, K_TICKS_FOREVER);
 
-K_THREAD_DEFINE(thread_b, STACK_SIZE,
-				Thread_B, NULL, NULL, NULL,
-				PRIO_THREAD_B, 0, K_TICKS_FOREVER);
+static void scan_done_handler(struct net_mgmt_event_callback *cb,
+                              uint32_t mgmt_event, struct net_if *iface)
+{
+    printk("\n--- Scan Completed ---\n\n");
+}
 
+static struct net_mgmt_event_callback scan_res_cb;
+static struct net_mgmt_event_callback scan_done_cb;
 
 int main(void)
 {
-	
-	k_thread_start(thread_a);
-	k_thread_start(thread_b);
+    struct net_if *iface = net_if_get_default();
+
+    net_mgmt_init_event_callback(&scan_res_cb, (net_mgmt_event_handler_t)scan_result_handler, NET_EVENT_WIFI_SCAN_RESULT);
+    net_mgmt_add_event_callback(&scan_res_cb);
+
+    net_mgmt_init_event_callback(&scan_done_cb, (net_mgmt_event_handler_t)scan_done_handler, NET_EVENT_WIFI_SCAN_DONE);
+    net_mgmt_add_event_callback(&scan_done_cb);
+
+    printk("\nWating for Wi-Fi interface to be ready...\n");
+    k_sleep(K_SECONDS(2)); 
+    net_if_up(iface);
+    k_sleep(K_MSEC(500)); 
+
+    printk("\nScanning for Wi-Fi networks...\n");
+
+    if (net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0)) {
+        printk("Failed to start scan!\n");
+        return -1;
+    }
+
+    while (1) {
+        k_sleep(K_SECONDS(10));
+    }
+
     return 0;
 }
